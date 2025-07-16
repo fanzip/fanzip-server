32f2e11045c78642845addae07d66c843ab190ff
Hyewon Lee <hooho931@gmail.com>
2025-07-16 16:06:13 +0900

    [chore] spring legacy 초기 설정 #1

---
+++ src/main/java/org/example/fanzip/config/RootConfig.java
@@ -0,0 +1,7 @@
+package org.example.fanzip.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+}
--- src/main/java/org/example/fanzip/controller/HomeController.java
+++ src/main/java/org/example/fanzip/controller/HomeController.java
@@ -1,12 +1,15 @@
 package org.example.fanzip.controller;
-import lombok.extern.log4j.Log4j2;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-@Controller
-public class HomeController {
 
+@Controller
+@Slf4j
+public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "index"; // 이렇게 하면 /WEB-INF/views/index.jsp 로 포워딩됨
+        log.info("================> HomeController /");
+        return "index";
     }
 }
