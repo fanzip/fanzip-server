c70842beab59591b9ef9ef1c89af44a898209825
Hyewon Lee <hooho931@gmail.com>
2025-07-21 23:02:22 +0900

    [fix] 결제 요청에 단일 foreign key 유효성 검사 로직 추가 #16

--- src/main/java/org/example/fanzip/payment/service/PaymentServiceImpl.java
+++ src/main/java/org/example/fanzip/payment/service/PaymentServiceImpl.java
@@ -3,7 +3,6 @@ package org.example.fanzip.payment.service;
 import lombok.RequiredArgsConstructor;
 import org.example.fanzip.payment.dto.PaymentsRequestDto;
 import org.example.fanzip.payment.dto.PaymentsResponseDto;
-import org.example.fanzip.payment.mapper.PaymentsMapper;
 import org.example.fanzip.payment.repository.PaymentsRepository;
 import org.springframework.stereotype.Service;
 import org.example.fanzip.payment.domain.Payments;
@@ -17,15 +16,29 @@ public class PaymentServiceImpl implements PaymentService{
 
     @Override
     public PaymentsResponseDto createPayment(PaymentsRequestDto requestDto) {
+        validateForeignKey(requestDto);
         Payments payments = requestDto.toEntity();
         paymentsRepository.save(payments);
         return PaymentsResponseDto.from(payments);
     }
 
-
     @Override
     public PaymentsResponseDto getPayment(Long paymentId) {
         Payments payments = paymentsRepository.findById(paymentId);
         return PaymentsResponseDto.from(payments);
+    }
+    /*
+        orderId, reservationId, membershipId null 확인 함수
+        충족 X -> IllegalArgumentException 발생
+     */
+    private void validateForeignKey(PaymentsRequestDto dto) {
+        int nonNullCount = 0;
+        if (dto.getOrderId() != null) nonNullCount++;
+        if (dto.getReservationId() != null) nonNullCount++;
+        if (dto.getMembershipId() != null) nonNullCount++;
+
+        if (nonNullCount != 1) {
+            throw new IllegalArgumentException("orderId, reservationId, membershipId 중 정확히 하나만 존재해야 한다.");
+        }
     }
 }
