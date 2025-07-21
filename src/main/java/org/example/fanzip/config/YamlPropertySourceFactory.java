036985360d12f3b6fe29a5c3d02743b2c4d88b52
Hyewon Lee <hooho931@gmail.com>
2025-07-21 16:21:23 +0900

    [feat] application.yml로 변경 #8

---
+++ src/main/java/org/example/fanzip/config/YamlPropertySourceFactory.java
@@ -0,0 +1,23 @@
+package org.example.fanzip.config;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+
+import java.util.Properties;
+
+public class YamlPropertySourceFactory implements PropertySourceFactory {
+
+    @Override
+    public PropertySource<?> createPropertySource(String name, EncodedResource resource) {
+        YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+        factory.setResources(resource.getResource());
+        Properties properties = factory.getObject();
+        return new PropertiesPropertySource(
+                name != null ? name : resource.getResource().getFilename(),
+                properties != null ? properties : new Properties()
+        );
+    }
+}
