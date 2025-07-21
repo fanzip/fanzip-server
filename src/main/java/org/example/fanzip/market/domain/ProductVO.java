987cd9c0d9a9a04c6c2aa96e9ddce941d7b6736b
dayeon1211 <dayeon1211m@gmail.com>
2025-07-21 16:47:57 +0900

    [feat] ProductVO 생성 #18

---
+++ src/main/java/org/example/fanzip/market/domain/ProductVO.java
@@ -0,0 +1,23 @@
+package org.example.fanzip.market.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+// 상품 목록 조회를 위한 VO
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductVO {
+    private Long productId;
+    private String name;
+    private BigDecimal price;
+    private BigDecimal discountedPrice;
+    private BigDecimal discountRate;
+    private String thumbnailImage;
+    private Boolean isSoldOut;
+}
