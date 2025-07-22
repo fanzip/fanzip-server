1472e463aabd8b43fe464ef77b32d8bdba9fa6f4
dayeon1211 <dayeon1211m@gmail.com>
2025-07-22 17:08:48 +0900

    [feat] MarketMapper 상세 상품 내역 조회 구현 #18

--- src/main/java/org/example/fanzip/market/mapper/MarketMapper.java
+++ src/main/java/org/example/fanzip/market/mapper/MarketMapper.java
@@ -9,16 +9,14 @@ import java.util.List;
 @Mapper
 public interface MarketMapper {
     // 모든 상품 목록 조회 (limit 개수만큼)
-    // - (param) limit: 조회 건수
-    // - (return) ProductVO 리스트
     List<MarketVO> getAllProducts(@Param("limit") int limit);
 
     // 마지막으로 가져온 productId 이후의 상품 조회
-    // - (param) lastProductId: 마지막으로 로드된 상품 ID (처음은 null 허용)
-    // - (param) limit: 조회 건수
-    // - (return) ProductVO 리스트
     List<MarketVO> getProductsAfter(
             @Param("lastProductId") Long lastProductId,
             @Param("limit") int limit
     );
+
+    // 상품 상세 페이지 조회
+    MarketVO findProductById(@Param("productId") Long productId);
 }
