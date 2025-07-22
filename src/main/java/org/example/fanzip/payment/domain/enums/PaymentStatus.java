0e8a47c785e29ad8a34ff44922bbda6baf390170
Hyewon Lee <hooho931@gmail.com>
2025-07-22 13:44:47 +0900

    [refactor] APROVED -> PAID로 승인/완료 병합 #25

--- src/main/java/org/example/fanzip/payment/domain/enums/PaymentStatus.java
+++ src/main/java/org/example/fanzip/payment/domain/enums/PaymentStatus.java
@@ -2,7 +2,6 @@ package org.example.fanzip.payment.domain.enums;
 
 public enum PaymentStatus {
     PENDING,    // 결제 요청됨
-    APPROVED, // 결제 승인
     PAID,       // 결제 완료
     CANCELLED,  // 유저 취소 or 유효 시간 만료
     FAILED,     // 결제 실패
