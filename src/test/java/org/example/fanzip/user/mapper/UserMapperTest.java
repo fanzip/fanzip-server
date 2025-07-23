d5933174cce5025fb06c09c808c1838a914657bf
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-07-23 14:20:31 +0900

    [test] 테스트 클래스(RootConfigTest, JwtProcessorTest, UserMapperTest, UserServiceTest)

---
+++ src/test/java/org/example/fanzip/user/mapper/UserMapperTest.java
@@ -0,0 +1,55 @@
+package org.example.fanzip.user.mapper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.fanzip.config.RootConfig;
+import org.example.fanzip.user.dto.AdditionalInfoDTO;
+import org.example.fanzip.user.dto.UserDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {RootConfig.class})
+@Slf4j
+class UserMapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    void findBySocialTypeAndSocialId() {
+        UserDTO userDTO = userMapper.findBySocialTypeAndSocialId("kakao", "12345");
+        log.info("userDTO={}", userDTO);
+        assertNotNull(userDTO);
+    }
+
+    @Test
+    void insertUser() {
+        UserDTO userDTO = UserDTO.builder()
+                .socialType("kakao")
+                .socialId("123456")
+                .created_at(new Date())
+                .build();
+        userMapper.insertUser(userDTO);
+
+    }
+
+    @Test
+    void updateAdditionalInfo() {
+//        AdditionalInfoDTO additionalInfoDTO = AdditionalInfoDTO.builder()
+//                .name("HongGilDong")
+//                .phone("010-1234-5678")
+//                .email("HongGilDong@gmail.com")
+//                .build();
+//        userMapper.updateAdditionalInfo("kakao", "12345",additionalInfoDTO);
+//        UserDTO userDTO = userMapper.findBySocialTypeAndSocialId("kakao", "12345");
+//        assertNotNull(userDTO);
+    }
+}
---
+++ src/test/java/org/example/fanzip/user/service/UserServiceTest.java
@@ -0,0 +1,50 @@
+package org.example.fanzip.user.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.fanzip.config.RootConfig;
+import org.example.fanzip.user.dto.AdditionalInfoDTO;
+import org.example.fanzip.user.dto.UserDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {RootConfig.class})
+@Slf4j
+class UserServiceTest {
+
+    @Autowired
+    private UserService service;
+
+    @Test
+    void findBySocialTypeAndSocialId() {
+        UserDTO userDTO = service.findBySocialTypeAndSocialId("kakao", "1234567");
+        log.info("userDTO={}", userDTO);
+        assertNotNull(userDTO);
+    }
+
+    @Test
+    void registerUser() {
+//        String socialType="kakao";
+//        String socialId="123456";
+//        service.loginOrRegister(socialType,socialId,null);
+
+    }
+
+    @Test
+    void updateAdditionalInfo() {
+//        AdditionalInfoDTO additionalInfoDTO = AdditionalInfoDTO.builder()
+//                .name("yonseeee")
+//                .phone("010-1234-5678")
+//                .email("yonseee@gmail.com")
+//                .build();
+//        service.updateAdditionalInfo("kakao","1234567", additionalInfoDTO);
+    }
+}
