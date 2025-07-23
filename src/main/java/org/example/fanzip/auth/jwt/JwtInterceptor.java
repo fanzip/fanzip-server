40c96d8a47361b143deacfe28c5b370313eaf04c
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-07-23 14:15:32 +0900

    [feat] Jwt 발급 및 검증 기능 (JwtProcessor, JwtInterceptor)

---
+++ src/main/java/org/example/fanzip/auth/jwt/JwtInterceptor.java
@@ -0,0 +1,45 @@
+package org.example.fanzip.auth.jwt;
+
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RequiredArgsConstructor
+public class JwtInterceptor implements HandlerInterceptor {
+
+    private final JwtProcessor jwtProcessor;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("jwtInterceptor preHandle");
+
+        String header=request.getHeader("Authorization");
+
+        if(header==null || !header.startsWith("Bearer ")){
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰이 없습니다.");
+            return false;
+        }
+
+        String token=header.substring(7);
+        System.out.println("token:"+token);
+        try{
+            Jws<Claims> claims= jwtProcessor.parseToken(token);
+
+            System.out.println("claims:"+claims);
+            String socialType=claims.getBody().get("socialType", String.class);
+            String socialId=claims.getBody().get("socialId", String.class);
+            request.setAttribute("socialType",socialType);
+            request.setAttribute("socialId",socialId);
+
+            return true;
+        }catch (JwtException e){
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않은 토큰입니다.");
+            return false;
+        }
+    }
+}
---
+++ src/main/java/org/example/fanzip/auth/jwt/JwtProcessor.java
@@ -0,0 +1,72 @@
+package org.example.fanzip.auth.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtProcessor {
+    static private final long TOKEN_VALID_MILISECOND=1000L*60*5;
+
+
+
+    private final Key key;
+
+    public JwtProcessor(@Value("${jwt.secret}") String secretKey) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+//    @Value("${jwt.secret}")
+//    private String secretKey;
+
+//    private String secretKey="SuperSecretKeyForJWTSigning123!@#";
+//    private Key key=Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+//    private Key key=Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    //JWT 생성
+    public String genereteToken(String socialType, String socialId){
+//        System.out.println("key:"+key);
+
+        return Jwts.builder()
+//                .setSubject(socialType+":"+socialId)
+                .claim("socialType",socialType)
+                .claim("socialId",socialId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime()+TOKEN_VALID_MILISECOND))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    public Jws<Claims> parseToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+    }
+
+    //JWT Subject(username) 추출
+    public String getUsername(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+    //JWT 검증(유효 기간 검증)-해석 불가인 경우 예외 발생
+    public boolean validateToken(String token){
+        Jws<Claims> claims=Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+        return true;
+    }
+}
