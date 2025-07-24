c6cc30c5f578ab6b36517e000c8dd395f4bbb022
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-07-24 13:28:49 +0900

    [refactor] JwtProcessorTest 수정

--- src/test/java/org/example/fanzip/auth/jwt/JwtProcessorTest.java
+++ src/test/java/org/example/fanzip/auth/jwt/JwtProcessorTest.java
@@ -20,30 +20,23 @@ class JwtProcessorTest {
 
     @Test
     void genereteToken() {
-        String socialType="kakao";
-        String socialId="1234";
-        String jwt=jwtProcessor.genereteToken(socialType,socialId);
+        String jwt=jwtProcessor.generateToken(3L);
         log.info("jwt:{}",jwt);
         assertNotNull(jwt);
-//        String token= jwtProcessor.genereteToken(username);
-//        log.info(token);
-//        assertNotNull(token);
     }
 
     @Test
-    void getUsername() {
-        String token="eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJrYWthbzoxMjM0IiwiaWF0IjoxNzUzMTY5NTEwLCJleHAiOjE3NTMxNjk4MTB9.KXplQCNqaK6r4UbAfpdTd9clTRE6T3HCeo8BdsRQ3cNsO1joHky5o8uQIxmdThe6";
-        String username=jwtProcessor.getUsername(token);
-        log.info(username);
-        assertNotNull(username);
+    void getUserId() {
+        String token= "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjksImlhdCI6MTc1MzMzMDQ1NSwiZXhwIjoxNzUzMzMyMjU1fQ.xrCrKujMwXBNo9Fk3C3j8KxBxWMoA6prkomxcTI3Qr4";
+        String userId=jwtProcessor.getUserId(token);
+        log.info("userId:{}",userId);
+        assertNotNull(userId);
     }
 
     @Test
     void validateToken() {
         String token="eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJ1c2VyMCIsImlhdCI6MTc1MzA3ODg1OCwiZXhwIjoxNzUzMDc5MTU4fQ.K1OUzZ6tk2g8bs8A6S_XtIQ8SASH_ZMy1Nk-YyxbiVHMzqLlQGhf3J7fKYjHGV-9";
-
         boolean isValid=jwtProcessor.validateToken(token);
-//        log.info(isValid);
         assertTrue(isValid);
     }
 }
