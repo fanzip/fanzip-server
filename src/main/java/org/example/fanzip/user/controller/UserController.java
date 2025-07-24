7aff394ac2e5270154fc59d16d2bfe5031e57d41
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-07-24 13:24:47 +0900

    [feat] jwt에서 socialType, socialId 제외, userId추가

--- src/main/java/org/example/fanzip/user/controller/UserController.java
+++ src/main/java/org/example/fanzip/user/controller/UserController.java
@@ -2,6 +2,7 @@ package org.example.fanzip.user.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.example.fanzip.auth.jwt.JwtProcessor;
 import org.example.fanzip.user.dto.AdditionalInfoDTO;
 import org.example.fanzip.user.service.UserService;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final JwtProcessor jwtProcessor;
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody AdditionalInfoDTO additionalInfoDTO){
-        userService.register(additionalInfoDTO);
-        return ResponseEntity.ok("sign up success!!");
+        Long userId=userService.register(additionalInfoDTO);
+        System.out.println(userId);
+        String jwt=jwtProcessor.generateToken(userId);
+        return ResponseEntity.ok()
+                .header("Authorization", "Bearer "+jwt)
+                .body("sign up successful");
     }
 }
