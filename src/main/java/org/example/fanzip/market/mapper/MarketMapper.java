c62f7035370f8e4e340bdf0d16f77bb4934de55c
dayeon1211 <dayeon1211m@gmail.com>
2025-07-25 19:50:13 +0900

    [fix] MarketVO를 ProductListDto와 ProductDetailDto로 분리 #18

--- src/main/java/org/example/fanzip/market/mapper/MarketMapper.java
+++ src/main/java/org/example/fanzip/market/mapper/MarketMapper.java
@@ -2,44 +2,49 @@ package org.example.fanzip.market.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.example.fanzip.market.domain.MarketVO;
+import org.example.fanzip.market.dto.ProductDetailDto;
+import org.example.fanzip.market.dto.ProductListDto;
 
 import java.util.List;
 
 @Mapper
 public interface MarketMapper {
     // 모든 상품 목록 조회 (limit 개수만큼)
-    List<MarketVO> getAllProducts(@Param("limit") int limit);
+    List<ProductListDto> getAllProducts(
+            @Param("limit") int limit);
 
     // 마지막으로 가져온 productId 이후의 상품 조회
-    List<MarketVO> getProductsAfter(
+    List<ProductListDto> getProductsAfter(
             @Param("lastProductId") Long lastProductId,
             @Param("limit") int limit
     );
+//
+//    // 등급 확인
+//    Integer findMyGrade(
+//            @Param("userId") Long userId,
+//            @Param("influencerId") Long influencerId
+//    );
 
     // 상품 상세 페이지 조회
-    MarketVO findProductById(@Param("productId") Long productId);
-
-    // 등급 확인
-    Integer findMyGrade(
-            @Param("userId") Long userId,
-            @Param("influencerId") Long influencerId
+    ProductDetailDto findProductById(
+            @Param("productId") Long productId,
+            @Param("userId") Long userId
     );
 
     // 검색 기능 (1페이지)
-    List<MarketVO> searchProducts(
+    List<ProductListDto> searchProducts(
             @Param("keyword") String keyword,
             @Param("limit") int limit
     );
 
     // 검색 기능
-    List<MarketVO> searchProductsAfter(
+    List<ProductListDto> searchProductsAfter(
             @Param("keyword") String keyword,
             @Param("lastProductId") Long lastProductId,
             @Param("limit") int limit
     );
 
     // 재고 조회
-    int getStock(@Param("productId") Long productId);
+    int getStock(
+            @Param("productId") Long productId);
 }
