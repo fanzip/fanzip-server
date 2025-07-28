7d8f7a2c166a2311db30e9b22a69ce46714c9610
Hyewon Lee <hooho931@gmail.com>
2025-07-28 17:08:22 +0900

    [refactor] PaymentValidator로 클래스 분리 #25

--- src/main/java/org/example/fanzip/payment/service/PaymentApproveService.java
+++ src/main/java/org/example/fanzip/payment/service/PaymentApproveService.java
@@ -15,6 +15,7 @@ import java.math.BigDecimal;
 public class PaymentApproveService {
     private final PaymentRepository paymentRepository;
     private final PaymentRollbackService paymentRollbackService;
+    private final PaymentValidator paymentValidator;
 
     @Transactional
     public PaymentResponseDto approvePaymentById(Long paymentId) {
@@ -22,7 +23,7 @@ public class PaymentApproveService {
         if (payments == null) {
             throw new IllegalArgumentException("해당 결제 정보를 찾을 수 없습니다. paymentId=" + paymentId);
         }
-        validateStockAvailability(payments.getOrderId(), payments.getReservationId(), payments.getMembershipId()); // 결제 승인 시 재고 수량 검사 홤수
+        paymentValidator.validateStockAvailability(payments.getOrderId(), payments.getReservationId(), payments.getMembershipId()); // 결제 승인 시 재고 수량 검사 홤수
         // TODO : 주문 금액과 결제 요청 금액이 맞는지 로직 변경 필요
         BigDecimal expectedAmount = getExpectedAmountMock(payments);
         System.out.println("expectedAmount : " + expectedAmount);
@@ -59,26 +60,26 @@ public class PaymentApproveService {
         return PaymentResponseDto.from(payments);
     }
 
-    protected void validateStockAvailability(Long orderId, Long reservationId, Long membershipId) { // 결제 요청 시 재고 수량 검사 홤수
-        if (orderId != null) {
-            int mockStock = 10; // 임의 재고 수량, 실제 구현 시 각 Repository Mapper에서 findById() 호출 하기
-            if (mockStock <= 0) {
-                throw new IllegalStateException("상품 재고가 부족합니다");
-            }
-        }
-        if (reservationId != null) { // 예매 가능 좌석
-            int mockSeats = 5;
-            if (mockSeats <= 0) {
-                throw new IllegalStateException("예약 가능한 인원이 없습니다");
-            }
-        }
-        if (membershipId != null) {
-            boolean isMember = true; // 멤버십 가입된 사람
-            if (!isMember) {
-                throw new IllegalStateException("멤버십 가입 정보가 없습니다.");
-            }
-        }
-    }
+//    protected void validateStockAvailability(Long orderId, Long reservationId, Long membershipId) { // 결제 요청 시 재고 수량 검사 홤수
+//        if (orderId != null) {
+//            int mockStock = 10; // 임의 재고 수량, 실제 구현 시 각 Repository Mapper에서 findById() 호출 하기
+//            if (mockStock <= 0) {
+//                throw new IllegalStateException("상품 재고가 부족합니다");
+//            }
+//        }
+//        if (reservationId != null) { // 예매 가능 좌석
+//            int mockSeats = 5;
+//            if (mockSeats <= 0) {
+//                throw new IllegalStateException("예약 가능한 인원이 없습니다");
+//            }
+//        }
+//        if (membershipId != null) {
+//            boolean isMember = true; // 멤버십 가입된 사람
+//            if (!isMember) {
+//                throw new IllegalStateException("멤버십 가입 정보가 없습니다.");
+//            }
+//        }
+//    }
 
     private BigDecimal getExpectedAmountMock(Payments payments){
         if (payments.getOrderId() != null) {
