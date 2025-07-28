96e36ae2d8901551914c97bbd5b9d3161762ce37
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-07-28 23:11:00 +0900

    [refactor] KakaoOAuthService: acccess token 리턴 방식 변경(DTO->String)

--- src/main/java/org/example/fanzip/auth/service/KakaoOAuthService.java
+++ src/main/java/org/example/fanzip/auth/service/KakaoOAuthService.java
@@ -3,7 +3,6 @@ package org.example.fanzip.auth.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.example.fanzip.auth.dto.KakaoTokenResponse;
 import org.example.fanzip.auth.dto.KakaoUserDTO;
 import org.example.fanzip.user.dto.UserDTO;
 import org.example.fanzip.user.service.UserService;
@@ -32,15 +31,14 @@ public class KakaoOAuthService implements OAuthService {
 
     @Override
     public KakaoUserDTO login(String code) throws Exception {
-        KakaoTokenResponse info=requestAccessToken(code);
-        JsonNode profile=requestUserInfo(info.getAccessToken());
+        String accessToken=requestAccessToken(code);
+        JsonNode profile=requestUserInfo(accessToken);
 
         String socialType="kakao";
         String socialId=profile.get("id").asText();
 
         JsonNode kakaoAccountNode=profile.path("kakao_account");
         String email=kakaoAccountNode.get("email").asText(null);
-        System.out.println("email:"+email);
 
         UserDTO user=userService.findBySocialTypeAndSocialId(socialType,socialId);
 
@@ -55,35 +53,8 @@ public class KakaoOAuthService implements OAuthService {
                 .userId(userId)
                 .build();
     }
-//    private KakaoTokenResponse refreshAccessToken(String socialId) throws IOException {
-//        String tokenUrl="https://kauth.kakao.com/oauth/token";
-//
-//        String refreshToken=userService.getRefreshtoken("kakao", socialId);
-//        if(refreshToken==null){throw new IllegalStateException("No refresh token found");}
-//
-//        RestTemplate restTemplate = new RestTemplate();
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-//
-//        MultiValueMap<String, String> params=new LinkedMultiValueMap<>();
-//        params.add("grant_type","refresh_token");
-//        params.add("client_id",clientId);
-//        params.add("refresh_token",refreshToken);
-//
-//        HttpEntity<MultiValueMap<String, String>> request=new HttpEntity<>(params,headers);
-//        ResponseEntity<String> response=restTemplate.postForEntity(tokenUrl,request,String.class);
-//
-//        JsonNode node=objectMapper.readTree(response.getBody());
-//        String newAccessToken=node.get("access_token").asText();
-//
-//        if(node.has("refresh_token")){
-//            String newRefreshToken=node.get("refresh_token").asText();
-//            userService.updateRefreshToken("kakao", newRefreshToken);
-//        }
-//        return newAccessToken;
-//    }
 
-    private KakaoTokenResponse requestAccessToken(String code) throws IOException {
+    private String requestAccessToken(String code) throws IOException {
         String tokenUrl="https://kauth.kakao.com/oauth/token";
 
         RestTemplate restTemplate = new RestTemplate();
@@ -102,12 +73,7 @@ public class KakaoOAuthService implements OAuthService {
         JsonNode node=objectMapper.readTree(response.getBody());
         System.out.println("카카오 응답 JSON: " + node.toPrettyString());
 
-        return new KakaoTokenResponse(
-                node.get("access_token").asText(),
-                node.has("refresh_token")?node.get("refresh_token").asText():null,
-                node.get("expires_in").asInt(),
-                node.has("refresh_token_expires_in")?node.get("refresh_token_expires_in").asInt():0
-        );
+        return node.get("access_token").asText();
     }
 
     private JsonNode requestUserInfo(String accessToken) throws IOException {
