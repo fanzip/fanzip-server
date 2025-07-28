c308a0c592fde4da3ad7fe8253af192b62e548e1
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-07-28 23:12:22 +0900

    [refactor] UserController: 회원가입 시 refresh token HttpOnly Cookie 설정

--- src/main/java/org/example/fanzip/user/controller/UserController.java
+++ src/main/java/org/example/fanzip/user/controller/UserController.java
@@ -3,8 +3,10 @@ package org.example.fanzip.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.fanzip.auth.jwt.JwtProcessor;
-import org.example.fanzip.user.dto.AdditionalInfoDTO;
+import org.example.fanzip.user.dto.RegisterDTO;
 import org.example.fanzip.user.service.UserService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,13 +21,26 @@ public class UserController {
     private final UserService userService;
     private final JwtProcessor jwtProcessor;
 
+    //회원가입
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody AdditionalInfoDTO additionalInfoDTO){
-        Long userId=userService.register(additionalInfoDTO);
-        System.out.println(userId);
-        String jwt=jwtProcessor.generateToken(userId);
+    public ResponseEntity<?> register(@RequestBody RegisterDTO registerDTO) throws Exception{
+
+        Long userId=userService.register(registerDTO);
+
+        String accessToken=jwtProcessor.generateAccessToken(userId);
+        String refreshToken=jwtProcessor.generateRefreshToken(userId);
+
+        ResponseCookie refreshCookie=ResponseCookie.from("refresh-token",refreshToken)
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(7*24*60*60)//7일
+                .sameSite("Lax")
+                .build();
+
         return ResponseEntity.ok()
-                .header("Authorization", "Bearer "+jwt)
-                .body("sign up successful");
+                .header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
+                .header("Authorization", "Bearer "+accessToken)
+                .body("sign up success");
     }
 }
