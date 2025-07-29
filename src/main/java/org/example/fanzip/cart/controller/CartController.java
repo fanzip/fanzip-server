fefef98a3408dcb2fa5082c666a2a7ebfdef9758
dayeon1211 <dayeon1211m@gmail.com>
2025-07-29 11:39:03 +0900

    [fix] jwt에서 추출된 userId 사용 #40

--- src/main/java/org/example/fanzip/cart/controller/CartController.java
+++ src/main/java/org/example/fanzip/cart/controller/CartController.java
@@ -8,52 +8,50 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.example.fanzip.auth.jwt.JwtProcessor;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
-    private final JwtProcessor jwtProcessor;
     private final CartService cartService;
 
     @Autowired
-    public CartController(CartService cartService, JwtProcessor jwtProcessor) {
+    public CartController(CartService cartService) {
         this.cartService = cartService;
-        this.jwtProcessor = jwtProcessor;
     }
 
     // 장바구니에 추가 - body: productId, quantity
     @PostMapping("/items")
     @ResponseStatus(HttpStatus.CREATED)
     public CartItemResponseDto addCartItem(
-//            @RequestHeader("Authorization") String header,
+//            HttpServletRequest request,
             @RequestHeader("X-USER-ID") Long userId,
             @RequestBody AddCartItemRequestDto req
             ) {
-//        String token = header.substring("Bearer ".length());
-//        Long userId = jwtProcessor.getUserId(token);
+
+//        Long userId = (Long) request.getAttribute("userId");
         return cartService.addItem(userId, req);
     }
 
     // 장바구니 물품 전체 조회
     @GetMapping("/items")
     public CartResponseDto getCartItems(
-//            @RequestHeader("Authorization") String header
+//             HttpServletRequest request
             @RequestHeader("X-USER-ID") Long userId
     ) {
-//        String token = header.substring("Bearer ".length());
-//        Long userId = jwtProcessor.getUserId(token);
+//        Long userId = (Long) request.getAttribute("userId");
         return cartService.getCart(userId);
     }
 
     // 개별 수량/선택 상태 변경 - quantity, isSelected
     @PutMapping("/items/{cartItemId}")
     public CartItemDto updateCartItem(
-//            @RequestHeader("Authorization") String header,
+//            HttpServletRequest request,
             @RequestHeader("X-USER-ID") Long userId,
             @PathVariable Long cartItemId,
             @RequestBody UpdateCartItemRequestDto req
     ) {
-//        String token = header.substring("Bearer ".length());
-//        Long userId = jwtProcessor.getUserId(token);
+//        Long userId = (Long) request.getAttribute("userId");
         return cartService.updateItem(userId, cartItemId, req);
     }
 
@@ -61,11 +59,11 @@ public class CartController {
     @PutMapping("/items/select-all")
     public CartResponseDto selectAll(
             @RequestHeader("X-USER-ID") Long userId,
-//            @RequestHeader("Authorization") String header,
+//            HttpServletRequest request,
             @RequestParam Boolean selectAll
     ) {
-//        String token = header.substring("Bearer ".length());
-//        Long userId = jwtProcessor.getUserId(token);
+
+//        Long userId = (Long) request.getAttribute("userId");
         return cartService.selectAll(userId, selectAll);
     }
 
@@ -73,12 +71,11 @@ public class CartController {
     @DeleteMapping("/items/{cartItemId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeItem(
-//            @RequestHeader("Authorization") String header,
+//            HttpServletRequest request,
             @RequestHeader("X-USER-ID") Long userId,
             @PathVariable Long cartItemId
     ) {
-//        String token = header.substring("Bearer ".length());
-//        Long userId = jwtProcessor.getUserId(token);
+//        Long userId = (Long) request.getAttribute("userId");
         cartService.removeItem(userId, cartItemId);
     }
 }
