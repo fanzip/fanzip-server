c0e61ed8b2dd62c58e807ccaa846b5a9b871301f
dayeon1211 <dayeon1211m@gmail.com>
2025-07-29 15:58:38 +0900

    [test] Market Service 테스트 작성 #40

---
+++ src/test/java/org/example/fanzip/market/service/MarketServiceImplTest.java
@@ -0,0 +1,55 @@
+package org.example.fanzip.market.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.fanzip.config.RootConfig;
+import org.example.fanzip.market.dto.ProductDetailDto;
+import org.example.fanzip.market.dto.ProductListDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {RootConfig.class})
+@Slf4j
+class MarketServiceImplTest {
+    @Autowired
+    private MarketService marketService;
+
+    @Test
+    void getAllProducts() {
+        List<ProductListDto> list = marketService.getAllProducts(10);
+        for (ProductListDto dto : list) {
+            log.info(dto.toString());
+        }
+    }
+
+    @Test
+    void getProductsAfter() {
+        List<ProductListDto> list = marketService.getProductsAfter(60L, 10);
+        for (ProductListDto dto : list) {
+            log.info(dto.toString());
+        }
+    }
+
+    @Test
+    void getProductDetail() {
+        ProductDetailDto dto = marketService.getProductDetail(1L, 5L);
+        log.info(dto.toString());
+        dto = marketService.getProductDetail(2L, 5L);
+        log.info(dto.toString());
+    }
+
+    @Test
+    void searchProducts() {
+        List<ProductListDto> list = marketService.searchProducts("상품", 30L, 20);
+        for (ProductListDto dto : list) {
+            log.info(dto.toString());
+        }
+    }
+}
