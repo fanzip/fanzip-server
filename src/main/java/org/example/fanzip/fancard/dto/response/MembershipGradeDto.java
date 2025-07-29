2bbdb305849db09103d757168e5a757aed1524b8
khanz0613 <khanz0613@naver.com>
2025-07-29 14:25:18 +0900

    [refactor] Fancard DTO를 Java record로 변환하여 불변성 및 간결성 향상 - FZ-13
    - MembershipGradeDto, BenefitDto를 Java 16+ record로 변환
    - Lombok 보일러플레이트 코드 제거로 코드 간결성 확보
    - record의 내재적 불변성으로 데이터 안전성 향상
    - @Builder 어노테이션 유지하여 기존 빌더 패턴 호환성 보장

--- src/main/java/org/example/fanzip/fancard/dto/response/MembershipGradeDto.java
+++ src/main/java/org/example/fanzip/fancard/dto/response/MembershipGradeDto.java
@@ -1,18 +1,11 @@
 package org.example.fanzip.fancard.dto.response;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class MembershipGradeDto {
-    private Long gradeId;
-    private String gradeName;
-    private String color;
-    private String benefitsDescription;
-}
+public record MembershipGradeDto(
+    Long gradeId,
+    String gradeName,
+    String color,
+    String benefitsDescription
+) {}
