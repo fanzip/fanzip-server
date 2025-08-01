24533afc292e17dd7afe30eeaf97dcb783599415
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-08-01 17:33:44 +0900

    [feat] SecurityInitializer 추가: Spring Security 필터 자동 등록

---
+++ src/main/java/org/example/fanzip/security/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package org.example.fanzip.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+//Spring Security 필터 자동 등록
+}
