50a91374a3b9f832636cd4a2483637126e363d01
AshtonSW <sprauncy76@gmail.com>
2025-08-01 15:42:42 +0900

    [feat] FanMeetingResponseDTO.java 구현

---
+++ src/main/java/org/example/fanzip/meeting/dto/FanMeetingResponseDTO.java
@@ -0,0 +1,25 @@
+package org.example.fanzip.meeting.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.fanzip.meeting.domain.FanMeetingStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FanMeetingResponseDTO {
+    private Long meetingId;
+    private String title;
+    private String venueName;
+    private String venueAddress;
+    private LocalDateTime meetingDate;
+    private int availableSeats;
+    private FanMeetingStatus status;
+    private LocalDateTime openTime;
+}
