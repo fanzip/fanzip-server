16961801eb6bae055e83639019a8989203df590d
Hyewon Lee <hooho931@gmail.com>
2025-08-03 21:19:31 +0900

    [chore] GlobalExceptioHandler 주석 처리 #38

--- src/main/java/org/example/fanzip/global/config/GlobalExceptionHandler.java
+++ src/main/java/org/example/fanzip/global/config/GlobalExceptionHandler.java
@@ -1,26 +1,26 @@
-package org.example.fanzip.global.config;
-
-import org.example.fanzip.fancard.exception.FancardNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(FancardNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleFancardNotFoundException(FancardNotFoundException ex) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.NOT_FOUND.value());
-        body.put("error", "Not Found");
-        body.put("message", ex.getMessage());
-        
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
-    }
-}
+//package org.example.fanzip.global.config;
+//
+//import org.example.fanzip.fancard.exception.FancardNotFoundException;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+//import java.time.LocalDateTime;
+//import java.util.LinkedHashMap;
+//import java.util.Map;
+//
+//@RestControllerAdvice
+//public class GlobalExceptionHandler {
+//
+//    @ExceptionHandler(FancardNotFoundException.class)
+//    public ResponseEntity<Map<String, Object>> handleFancardNotFoundException(FancardNotFoundException ex) {
+//        Map<String, Object> body = new LinkedHashMap<>();
+//        body.put("timestamp", LocalDateTime.now());
+//        body.put("status", HttpStatus.NOT_FOUND.value());
+//        body.put("error", "Not Found");
+//        body.put("message", ex.getMessage());
+//
+//        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+//    }
+//}
