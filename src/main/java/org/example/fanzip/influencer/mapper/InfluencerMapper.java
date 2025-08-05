3bea238d12711f474f47e26a1708bec50cae738d
Hyewon Lee <hooho931@gmail.com>
2025-08-06 00:56:45 +0900

    [feat] InfluencerMapper.java 구현 #75

--- src/main/java/org/example/fanzip/influencer/mapper/InfluencerMapper.java
+++ src/main/java/org/example/fanzip/influencer/mapper/InfluencerMapper.java
@@ -4,7 +4,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.example.fanzip.influencer.domain.InfluencerVO;
 import org.example.fanzip.influencer.domain.enums.InfluencerCategory;
-import org.example.fanzip.influencer.dto.InfluencerDetailResponseDTO;
 
 import java.util.List;
 
@@ -16,5 +15,16 @@ public interface InfluencerMapper {
 
     InfluencerVO findDetailed(@Param("influencerId") Long influencerId);
 
+    // 프로필 조회
+    InfluencerVO findProfile(@Param("influencerId") Long influencerId);
 
+    // 프로필 수정
+    int updateProfile(@Param("influencerId") Long influencerId,
+                      @Param("influencerName") String influencerName,
+                      @Param("description") String description,
+                      @Param("category") InfluencerCategory category);
+
+    // 프로필 이미지 수정
+    int updateProfileImage(@Param("influencerId") Long influencerId,
+                           @Param("profileImage") String profileImage);
 }
