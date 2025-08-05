15be1a10b7ad9d96a1bcf455f2dbe0dbdd7d9ed9
Hyewon Lee <hooho931@gmail.com>
2025-08-06 01:00:40 +0900

    [feat] 인플루언서 프로필 수정 요청 DTO 추가 #75

---
+++ src/main/java/org/example/fanzip/influencer/dto/InfluencerProfileImgUpdateRequestDTO.java
@@ -0,0 +1,12 @@
+package org.example.fanzip.influencer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InfluencerProfileImgUpdateRequestDTO {
+    private String profileImage;
+}
