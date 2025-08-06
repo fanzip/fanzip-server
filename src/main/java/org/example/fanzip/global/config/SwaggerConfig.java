f377804ddb108d17ed6d5e78d7c3c32e8ecb45e6
khanz0613 <khanz0613@naver.com>
2025-08-06 10:24:33 +0900

    [fix] swagger 오류 해결

--- src/main/java/org/example/fanzip/global/config/SwaggerConfig.java
+++ src/main/java/org/example/fanzip/global/config/SwaggerConfig.java
@@ -2,13 +2,18 @@ package org.example.fanzip.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -21,7 +26,9 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("org.example.fanzip"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .securitySchemes(Arrays.asList(apiKeyScheme()))
+                .securityContexts(Arrays.asList(securityContext()));
     }
 
     private ApiInfo apiInfo() {
@@ -30,5 +37,23 @@ public class SwaggerConfig {
                 .description("Fanzip 서비스 API 문서")
                 .version("1.0")
                 .build();
+    }
+
+    private ApiKey apiKeyScheme() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/api/.*"))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }
