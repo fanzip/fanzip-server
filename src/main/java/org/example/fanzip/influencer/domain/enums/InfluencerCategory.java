d68abcf0c08d4c1433b33b363ba8ee28fa88b118
Hyewon Lee <hooho931@gmail.com>
2025-08-06 11:34:11 +0900

    [refactor] Category 키워드 영어로 변경 #75

--- src/main/java/org/example/fanzip/influencer/domain/enums/InfluencerCategory.java
+++ src/main/java/org/example/fanzip/influencer/domain/enums/InfluencerCategory.java
@@ -1,19 +1,19 @@
 package org.example.fanzip.influencer.domain.enums;
 
 public enum InfluencerCategory {
-    뷰티,
-    게임,
-    일상,
-    패션,
-    요리,
-    다이어트_건강,
-    반려동물,
-    키즈,
-    교육_지식,
-    여행,
-    음악,
-    운동,
-    스포츠,
-    언어,
-    기타
+    BEAUTY,        // 뷰티
+    GAME,          // 게임
+    DAILY,         // 일상
+    FASHION,       // 패션
+    COOKING,       // 요리
+    HEALTH,        // 다이어트/건강
+    PET,           // 반려동물
+    KIDS,          // 키즈
+    EDUCATION,     // 교육/지식
+    TRAVEL,        // 여행
+    MUSIC,         // 음악
+    FITNESS,       // 운동
+    SPORTS,        // 스포츠
+    LANGUAGE,      // 언어
+    ETC            // 기타
 }
