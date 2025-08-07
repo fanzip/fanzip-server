47b42ef18021c7c915eca991f2ffc073824e79d3
AshtonSW <sprauncy76@gmail.com>
2025-08-07 11:36:44 +0900

    [feat] 팬미팅 관련 insert 및 상세 조회 쿼리 추가
    - fan_meetings 테이블에 데이터를 삽입하는 insertFanMeeting 쿼리 추가
    - meeting_id로 상세 정보를 조회하는 findDetailById 쿼리 추가
    - findDetailById는 DTO에 매핑하기 위해 컬럼 별칭을 지정하여 상세 정보를 반환

--- src/main/java/org/example/fanzip/meeting/mapper/FanMeetingMapper.java
+++ src/main/java/org/example/fanzip/meeting/mapper/FanMeetingMapper.java
@@ -1,10 +1,14 @@
 package org.example.fanzip.meeting.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.example.fanzip.meeting.domain.FanMeetingVO;
+import org.example.fanzip.meeting.dto.FanMeetingDetailResponseDTO;
 
 import java.util.List;
 
 public interface FanMeetingMapper {
     List<FanMeetingVO> findAllOpenMeetings();
     FanMeetingVO findById(Long meetingId);
+    void insertFanMeeting(FanMeetingVO fanMeetingVO);
+    FanMeetingDetailResponseDTO findDetailById(@Param("meetingId") Long meetingId);
 }
