98f74fb300d54ffcfb94a9960de7ca344ea83309
dayeon1211 <dayeon1211m@gmail.com>
2025-08-10 16:19:58 +0900

    [feat] 사용자 배송 정보 추가 #60

--- src/main/java/org/example/fanzip/cart/dto/UserShippingInfoDto.java
+++ src/main/java/org/example/fanzip/cart/dto/UserShippingInfoDto.java
@@ -1,22 +1,13 @@
 package org.example.fanzip.cart.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CartResponseDto {
-    private List<CartItemDto> items;
-    // 선택 항목만 최종 금액
-    private BigDecimal grandTotal;
-
+public class UserShippingInfoDto {
     // 유저 정보 (배송관련)
     private String address1;
     private String address2;
