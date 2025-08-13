6939ce48490a658bcc702d152a4213f33f2c5a82
AshtonSW <sprauncy76@gmail.com>
2025-08-14 00:37:34 +0900

    [refactor] 주석 제거

--- src/main/java/org/example/fanzip/membership/service/MembershipService.java
+++ src/main/java/org/example/fanzip/membership/service/MembershipService.java
@@ -24,13 +24,7 @@ public interface MembershipService{
      */
     List<MembershipGradeDTO> getMembershipGrades();
     
-    /**
-     * 사용자의 현재 등급 조회 (구독중인 멤버십 중 최고 등급)
-     */
     UserMembershipInfoDTO getUserMembershipInfo(Long userId);
-    
-    /**
-     * 특정 인플루언서에 대한 사용자의 구독 정보 조회
-     */
+
     UserMembershipInfoDTO.UserMembershipSubscriptionDTO getUserSubscriptionByInfluencer(Long userId, Long influencerId);
 }
