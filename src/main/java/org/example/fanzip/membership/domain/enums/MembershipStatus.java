f23aa6eb387707e45a745b69ef634337de62656a
joojinhyeok <jjh68979@gmail.com>
2025-08-14 17:31:40 +0900

    [feat] 멤버십 결제 연동 #136

--- src/main/java/org/example/fanzip/membership/domain/enums/MembershipStatus.java
+++ src/main/java/org/example/fanzip/membership/domain/enums/MembershipStatus.java
@@ -1,6 +1,7 @@
 package org.example.fanzip.membership.domain.enums;
 
 public enum  MembershipStatus {
+    PENDING,    // 결제 대기 상태
     ACTIVE,     // 구독 활성화
     CANCELLED,  // 결제 실패
     FAILED      // 유저 취소
