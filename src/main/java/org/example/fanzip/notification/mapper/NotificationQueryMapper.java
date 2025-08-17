2670fafabff48baaeb24b13f24f32af8a57acf57
joojinhyeok <jjh68979@gmail.com>
2025-08-17 15:46:49 +0900

    [feat] 마켓 등록 시 알림 발송 매퍼 추가 #142

--- src/main/java/org/example/fanzip/notification/mapper/NotificationQueryMapper.java
+++ src/main/java/org/example/fanzip/notification/mapper/NotificationQueryMapper.java
@@ -3,7 +3,10 @@ package org.example.fanzip.notification.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 @Mapper
 public interface NotificationQueryMapper {
     String findInfluencerNameById(@Param("influencerId") Long influencerId);
+
 }
